36632d1ae828d9e726f5d8f63a2e8092b353d995
Su1tan05 <Su1tan05@yandex.ru>
2022-12-22 20:22:27 +0300

    add multiple motors support

--- ros_delta_firmware/Libs/config.h
+++ ros_delta_firmware/Libs/config.h
@@ -1,10 +1,24 @@
 #ifndef CONFIG_H
 #define CONFIG_H
-#define PWM_PIN D5
-#define IN1_PIN D4
-#define IN2_PIN D3
-#define ENCA_PIN D6
-#define ENCB_PIN D7
+// motor1
+#define M1_PWM_PIN D5
+#define M1_IN1_PIN D4
+#define M1_IN2_PIN D3
+#define M1_ENCA_PIN D6
+#define M1_ENCB_PIN D7
+// motor2
+#define M2_PWM_PIN D9
+#define M2_IN1_PIN D8
+#define M2_IN2_PIN D12
+#define M2_ENCA_PIN D10
+#define M2_ENCB_PIN D11
+// motor3
+#define M3_PWM_PIN PB_0
+#define M3_IN1_PIN PA_4
+#define M3_IN2_PIN PA_1
+#define M3_ENCA_PIN PC_12
+#define M3_ENCB_PIN PC_10
+
 #define REDUCTER_RATIO 100
 #define ENCODER_IMPULSES 16
 #endif
--- ros_delta_firmware/main.cpp
+++ ros_delta_firmware/main.cpp
@@ -16,36 +16,68 @@
 float kp = 5;
 float ki = 0.5;
 float kd = 0;
-float setpoint = 0;
 
-Ticker motor_info_ticker;
+float setpoint1 = 0;
+float setpoint2 = 0;
+float setpoint3 = 0;
 
-Encoder encoder{ENCA_PIN, ENCB_PIN, REDUCTER_RATIO, ENCODER_IMPULSES, true};
+Ticker Motor1_info_ticker;
+Encoder Mortor1_encoder{M1_ENCA_PIN, M1_ENCB_PIN, REDUCTER_RATIO, ENCODER_IMPULSES, true};
+DcServo Motor1{M1_IN1_PIN, M1_IN2_PIN, M1_PWM_PIN, &Mortor1_encoder};
 
-DcServo servo{IN1_PIN, IN2_PIN, PWM_PIN, &encoder};
+Ticker Motor2_info_ticker;
+Encoder Mortor2_encoder{M2_ENCA_PIN, M2_ENCB_PIN, REDUCTER_RATIO, ENCODER_IMPULSES, true};
+DcServo Motor2{M2_IN1_PIN, M2_IN2_PIN, M2_PWM_PIN, &Mortor1_encoder};
+
+Ticker Motor3_info_ticker;
+Encoder Mortor3_encoder{M3_ENCA_PIN, M3_ENCB_PIN, REDUCTER_RATIO, ENCODER_IMPULSES, true};
+DcServo Motor3{M3_IN1_PIN, M3_IN2_PIN, M3_PWM_PIN, &Mortor1_encoder};
 
 ros::NodeHandle nh;
-geometry_msgs::Vector3 monitoring_msg;
 
-void pwmSub(const std_msgs::Float32 &pwm_msg);
-void setAngle(const std_msgs::Float32 &angle_msg);
-void pidTuningsCb(const geometry_msgs::Vector3 &pid_tunings_msg);
-void stopMotor(const std_msgs::Empty &stop_msg);
-void getMotorInfo(void);
+geometry_msgs::Vector3 monitoring_msg_m1;
+geometry_msgs::Vector3 monitoring_msg_m2;
+geometry_msgs::Vector3 monitoring_msg_m3;
+
 void initRosTopics(void);
+void stopMotor(const std_msgs::Int32 &stop_msg);
 
-ros::Subscriber<std_msgs::Float32> motor_pwm_sub("motor_pwm", &pwmSub);
-ros::Subscriber<std_msgs::Empty> stop_motor_sub("stop_motor", &stopMotor);
-ros::Subscriber<std_msgs::Float32> set_angle_sub("set_angle", &setAngle);
-ros::Subscriber<geometry_msgs::Vector3> pid_tunings_sub("pid_tunings", &pidTuningsCb);
+void setAngleMotor1(const std_msgs::Float32 &angle_msg);
+void pidTuningsMotor1(const geometry_msgs::Vector3 &pid_tunings_msg);
+void getInfoMotor1(void);
 
-ros::Publisher monitoring_pub("monitoring", &monitoring_msg);
+void setAngleMotor2(const std_msgs::Float32 &angle_msg);
+void pidTuningsMotor2(const geometry_msgs::Vector3 &pid_tunings_msg);
+void getInfoMotor2(void);
+
+void setAngleMotor3(const std_msgs::Float32 &angle_msg);
+void pidTuningsMotor3(const geometry_msgs::Vector3 &pid_tunings_msg);
+void getInfoMotor3(void);
+
+ros::Subscriber<std_msgs::Int32> stop_motor_sub("stop_motor1", &stopMotor);
+
+ros::Subscriber<std_msgs::Float32> motor1_set_angle_sub("set_angle_motor1", &setAngleMotor1);
+ros::Subscriber<geometry_msgs::Vector3> motor1_pid_tunings_sub("pid_tunings_motor1", &pidTuningsMotor1);
+
+ros::Subscriber<std_msgs::Float32> motor2_set_angle_sub("set_angle_motor2", &setAngleMotor2);
+ros::Subscriber<geometry_msgs::Vector3> motor2_pid_tunings_sub("pid_tunings_motor2", &pidTuningsMotor2);
+
+ros::Subscriber<std_msgs::Float32> motor3_set_angle_sub("set_angle_motor3", &setAngleMotor3);
+ros::Subscriber<geometry_msgs::Vector3> motor3_pid_tunings_sub("pid_tunings_motor3", &pidTuningsMotor3);
+
+ros::Publisher m1_monitoring_pub("monitoring_m1", &monitoring_msg_m1);
+ros::Publisher m2_monitoring_pub("monitoring_m2", &monitoring_msg_m2);
+ros::Publisher m3_monitoring_pub("monitoring_m3", &monitoring_msg_m3);
 
 int main()
 {
     nh.initNode();
     initRosTopics();
-    motor_info_ticker.attach(getMotorInfo, PUB_RATE);
+
+    Motor1_info_ticker.attach(getInfoMotor1, PUB_RATE);
+    Motor2_info_ticker.attach(getInfoMotor2, PUB_RATE);
+    Motor3_info_ticker.attach(getInfoMotor3, PUB_RATE);
+
     while (1)
     {
         nh.spinOnce();
@@ -54,42 +86,101 @@ int main()
 
 void initRosTopics()
 {
-    nh.advertise(monitoring_pub);
-    nh.subscribe(motor_pwm_sub);
     nh.subscribe(stop_motor_sub);
-    nh.subscribe(set_angle_sub);
-    nh.subscribe(pid_tunings_sub);
+
+    nh.advertise(m1_monitoring_pub);
+    nh.subscribe(motor1_set_angle_sub);
+    nh.subscribe(motor1_pid_tunings_sub);
+
+    nh.advertise(m2_monitoring_pub);
+    nh.subscribe(motor2_set_angle_sub);
+    nh.subscribe(motor2_pid_tunings_sub);
+
+    nh.advertise(m3_monitoring_pub);
+    nh.subscribe(motor3_set_angle_sub);
+    nh.subscribe(motor3_pid_tunings_sub);
 }
 
-void pwmSub(const std_msgs::Float32 &pwm_msg)
+void stopMotor(const std_msgs::Int32 &stop_msg)
 {
-    servo.revolute(pwm_msg.data);
-    monitoring_msg.y = pwm_msg.data;
+    switch (stop_msg.data)
+    {
+        case 1:
+            setpoint1 = 0;
+            Motor1.stop();
+            break;
+        case 2:
+            setpoint2 = 0;
+            Motor2.stop();
+            break;
+        case 3:
+            setpoint3 = 0;
+            Motor3.stop();
+            break;
+        default:
+            break;
+    }
 }
 
-void stopMotor(const std_msgs::Empty &stop_msg)
-{
-    setpoint = 0;
-    servo.stop();
+void setAngleMotor1(const std_msgs::Float32 &angle_msg){
+    setpoint1 = angle_msg.data;
+    monitoring_msg_m1.x = setpoint1;
+    Motor1.setAngle(setpoint1);
 }
 
-void setAngle(const std_msgs::Float32 &angle_msg){
-    setpoint = angle_msg.data;
-    monitoring_msg.x = setpoint;
-    servo.setAngle(setpoint);
+void setAngleMotor2(const std_msgs::Float32 &angle_msg){
+    setpoint2 = angle_msg.data;
+    monitoring_msg_m2.x = setpoint2;
+    Motor2.setAngle(setpoint2);
 }
 
-void pidTuningsCb(const geometry_msgs::Vector3 &pid_tunings_msg)
+void setAngleMotor3(const std_msgs::Float32 &angle_msg){
+    setpoint3 = angle_msg.data;
+    monitoring_msg_m3.x = setpoint3;
+    Motor3.setAngle(setpoint3);
+}
+
+void pidTuningsMotor1(const geometry_msgs::Vector3 &pid_tunings_msg)
 {
     kp = pid_tunings_msg.x;
     ki = pid_tunings_msg.y;
     kd = pid_tunings_msg.z;
-    servo.setPid(kp, ki, kd);
+    Motor1.setPid(kp, ki, kd);
 }
 
-void getMotorInfo(){
-    monitoring_msg.x = setpoint;
-    monitoring_msg.y = servo.getAngle();
-    monitoring_msg.z = servo.getPWM();
-    monitoring_pub.publish(&monitoring_msg);
+void pidTuningsMotor2(const geometry_msgs::Vector3 &pid_tunings_msg)
+{
+    kp = pid_tunings_msg.x;
+    ki = pid_tunings_msg.y;
+    kd = pid_tunings_msg.z;
+    Motor2.setPid(kp, ki, kd);
+}
+
+void pidTuningsMotor3(const geometry_msgs::Vector3 &pid_tunings_msg)
+{
+    kp = pid_tunings_msg.x;
+    ki = pid_tunings_msg.y;
+    kd = pid_tunings_msg.z;
+    Motor3.setPid(kp, ki, kd);
+}
+
+void getInfoMotor1(){
+    monitoring_msg_m1.x = setpoint1;
+    monitoring_msg_m1.y = Motor1.getAngle();
+    monitoring_msg_m1.z = Motor1.getPWM();
+    m1_monitoring_pub.publish(&monitoring_msg_m1);
+}
+
+void getInfoMotor2(){
+    monitoring_msg_m2.x = setpoint2;
+    monitoring_msg_m2.y = Motor2.getAngle();
+    monitoring_msg_m2.z = Motor2.getPWM();
+    m2_monitoring_pub.publish(&monitoring_msg_m2);
+}
+
+void getInfoMotor3(){
+    monitoring_msg_m3.x = setpoint3;
+    monitoring_msg_m3.y = Motor3.getAngle();
+    monitoring_msg_m3.z = Motor3.getPWM();
+    m3_monitoring_pub.publish(&monitoring_msg_m3);
 }
